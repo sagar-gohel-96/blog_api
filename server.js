85667b3e4d16fba4629fba85d2d54f6e638df2f0
sagar_gohel <sagar.180180107011@gmail.com>
2022-12-13 15:19:33 +0530

    setup : blog_api

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    case "/about-me":
+      res.writeHead(301, { Location: "/about" });
+      res.end();
+      break;
+    default:
+      path += "404.html";
+      break;
+  } 
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFile(path, (err, data) => {
+    if (err) throw err;
+    else {
+      res.end(data);
+    }
+  });
+});
+server.listen(3000, "localhost", () => {
+  console.log("server listening on port 3000");
+});
