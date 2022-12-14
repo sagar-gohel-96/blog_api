26593daaa930794ce83d38dc1356c0c4437b1846
sagar_gohel <sagar.180180107011@gmail.com>
2022-12-14 16:09:28 +0530

    done !

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,30 +3,6 @@ const router = express.Router();
 const Blog = require("../models/blog");
 const blogController = require("../controllers/blogController")
 const app = express();
-router.get("/add-blog", (req, res) => {
-  const blog = new Blog({
-    title: "new blog4",
-    snippet: "jungleees",
-    body: "house of animals",
-  });
-  blog
-    .save()
-    .then((result) => res.send(result))
-    .catch((err) => console.log(err));
-});
-//get single blog route
-router.get("/single-blogs", (req, res) => {
-  Blog.findById("63997295e36bcd18a20b1d12")
-    .then((result) => res.send(result))
-    .catch((err) => console.log(err));
-});
-
-//get all blog route
-router.get("/all-blogs", (req, res) => {
-  Blog.find()
-    .then((result) => res.send(result))
-    .catch((err) => console.log(err));
-});
 
 // regester view engine
 app.set("view engine", "ejs");
